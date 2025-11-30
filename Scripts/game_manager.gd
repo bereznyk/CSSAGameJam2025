5ec05e0df94934442bd6bdeef6b2397910085c37
ACaesarSalad <82750710+ACaesarSalad@users.noreply.github.com>
2025-11-29 19:53:32 -0600

    remove placeholder shapes and enemies from map

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,6 +3,9 @@ extends Node
 var game_over = false
 var score = 0
 
+var num_shapes = 0 # Number of shapes on the map
+var num_enemies = 0 # Number of enemies on the map
+
 @onready var game_score = %GameScore
 @onready var death_screen = %Death
 @onready var end_score = %EndScore
@@ -11,6 +14,9 @@ func _process(delta):
 	if Input.is_action_just_pressed("restart") and game_over:
 		get_tree().paused = false
 		get_tree().reload_current_scene()
+
+func start_game():
+	pass
 
 func end_game():
 	game_over = true
